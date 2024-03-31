ea2e25f1718bad21b209de1a35ede9a5d2f78d74
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2024-03-31 00:48:47 -0300

    An important UI improvement

--- Dicee-iOS/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS/Dicee-iOS13/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
     
+    @IBOutlet weak var theHouseValueLabel: UILabel!
+    @IBOutlet weak var youValueLabel: UILabel!
+    
+    @IBOutlet weak var rollButton: UIButton!
+
     var arrayDiceImages = [
         UIImage(imageLiteralResourceName: "DiceOne"),
         UIImage(imageLiteralResourceName: "DiceTwo"),
@@ -23,11 +28,21 @@ class ViewController: UIViewController {
     
     var leftDiceNumber = 0
     var rightDiceNumber = 5
+    var theHouseValue = 0
+    var youValue = 0
+    
+    func compareNumbersOfDices(nroA: Int, nroB: Int)-> Bool {
+        let result = nroA == nroB
+        return result
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         diceImageViewOne.image = arrayDiceImages[leftDiceNumber]
         diceImageViewTwo.image = arrayDiceImages[rightDiceNumber]
+        theHouseValueLabel.text = "\(theHouseValue)"
+        youValueLabel.text = "\(youValue)"
+        
     }
     
     func turnImageView(_ imageView: UIImageView, withRandomNumber randomNumber: Int) {
@@ -44,15 +59,32 @@ class ViewController: UIViewController {
             imageView.transform = .identity
             imageView.image = self.arrayDiceImages[randomNumber]
         }
-    } 
+    }
     
-    @IBAction func RollButtonPressed(_ sender: Any) {
+    @IBAction func RollButtonDown(_ sender: Any) {
+        rollButton.backgroundColor = .clear
+        rollButton.setTitle("⏳", for: .normal)
+    }
+    
+    @IBAction func RollButtonPressed(_ sender: UIButton) {
         let leftRandomNumber = Int.random(in: 0...5)
         let rightRandomNumber = Int.random(in: 0...5)
-        print("Random Number Left: \(leftRandomNumber)")
-        print("Random Number Right: \(rightRandomNumber)")
-        
+                
         turnImageView(diceImageViewOne, withRandomNumber: leftRandomNumber)
         turnImageView(diceImageViewTwo, withRandomNumber: rightRandomNumber)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.25) {
+            self.rollButton.backgroundColor = UIColor.systemGreen
+            self.rollButton.setTitle("ROLL THE DICE", for: .normal)
+            let resultComparision = self.compareNumbersOfDices(nroA: leftRandomNumber, nroB: rightRandomNumber)
+            if resultComparision == true {
+                let newScore = leftRandomNumber + rightRandomNumber + 2
+                self.youValue = self.youValue + newScore
+                self.youValueLabel.text = "\(self.youValue)"
+            } else {
+                self.theHouseValue = self.theHouseValue + 1
+                self.theHouseValueLabel.text = "\(self.theHouseValue)"
+            }
+        }
+
     }
 }
