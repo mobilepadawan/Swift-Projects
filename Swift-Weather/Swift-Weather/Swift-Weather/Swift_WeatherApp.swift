39ff22ffb994fb81f803fa5c0f06fb20cba6e26e
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2024-04-05 13:49:58 -0300

    Building the classic weather app

---
+++ Swift-Weather/Swift-Weather/Swift-Weather/Swift_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_WeatherApp.swift
+//  Swift-Weather
+//
+//  Created by Fernando Luna on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
